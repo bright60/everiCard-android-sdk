37a7b83b313f41adee02a15fa593baedb95f3091
Fei Liu <liu.feiwood@gmail.com>
2019-07-22 17:07:28 +0200

    bring back some code

--- sdk/src/main/java/ltd/vastchain/evericard/sdk/Card.java
+++ sdk/src/main/java/ltd/vastchain/evericard/sdk/Card.java
@@ -1,7 +1,11 @@
 package ltd.vastchain.evericard.sdk;
 
+import java.util.Arrays;
+
 import io.everitoken.sdk.java.PublicKey;
 import ltd.vastchain.evericard.sdk.channels.EveriCardChannel;
+import ltd.vastchain.evericard.sdk.command.Command;
+import ltd.vastchain.evericard.sdk.utils.CardResponseParser;
 
 public class Card {
     private EveriCardChannel channel;
@@ -11,17 +15,16 @@ public class Card {
     }
 
     public PublicKey getPublicKey(int keyIndex) throws VCChipException {
-//        String str_send = "800b000041";
-//
-//        byte[] ret = channel.sendCommand(HexHelper.hexTobyteArray(str_send));
-//        short statusCode = CardResponseParser.getResponseStatusCode(ret);
-//
-//        if (statusCode == (short) 0x9000) {
-//            PublicKey key = new PublicKey(Arrays.copyOfRange(ret, 0, ret.length - 2));
-//            return key;
-//        } else {
-//            throw new VCChipException("get_publicKey_fail", "could not get public key");
-//        }
-        return PublicKey.of("");
+        Command command = new Command((byte) 0x0b, (byte) 0x00, (byte) 0x00, (byte) 0x41); //"800b000041";
+
+        byte[] ret = channel.sendCommand(command);
+        short statusCode = CardResponseParser.getResponseStatusCode(ret);
+
+        if (statusCode == (short) 0x9000) {
+            PublicKey key = new PublicKey(Arrays.copyOfRange(ret, 0, ret.length - 2));
+            return key;
+        } else {
+            throw new VCChipException("get_publicKey_fail", "could not get public key");
+        }
     }
 }
