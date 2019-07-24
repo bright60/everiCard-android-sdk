f0463c1eacfc9aa99c7c5e15a3c108621f049b9a
Fei Liu <liu.feiwood@gmail.com>
2019-07-24 17:04:37 +0200

    add api to card
    * getPublicKeyByIndex
    * getDisplayName
    * setDisplayName

--- sdk-demo/src/main/java/ltd/vastchain/sdk_demo/MainActivity.java
+++ sdk-demo/src/main/java/ltd/vastchain/sdk_demo/MainActivity.java
@@ -1,7 +1,11 @@
 package ltd.vastchain.sdk_demo;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,14 +16,57 @@ import ltd.vastchain.evericard.sdk.VCChipException;
 
 public class MainActivity extends AppCompatActivity implements CardManager.OnCardSwipeListener {
     private CardManager cardManager;
+    private Button getPubKey0;
+    private Button getDisplayName;
+    private Button setDisplayName;
+    private EditText commandInput;
+    private TextView outputText;
+    private Card card;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_main);
+        getPubKey0 = (Button) findViewById(R.id.getPubKey0);
+        getDisplayName = (Button) findViewById(R.id.getDisplayName);
+        setDisplayName = (Button) findViewById(R.id.setDisplayName);
+        outputText = (TextView) findViewById(R.id.outputText);
+        outputText.setTextIsSelectable(true);
 
         this.cardManager = new CardManager(this);
+        getPubKey0.setOnClickListener((v) -> {
+            this.handleClick(this, "get_pubkey_0");
+        });
+        getDisplayName.setOnClickListener((v) -> {
+            this.handleClick(this, "get_display_name");
+        });
+        setDisplayName.setOnClickListener((v) -> {
+            this.handleClick(this, "set_display_name");
+        });
+
         cardManager.setOnCardSwipeListener(this);
+    }
+
+    private void handleClick(Context ctx, String command) {
+        try {
+            if (card != null) {
+                if (command.equals("get_pubkey_0")) {
+                    this.outputText.setText(card.getPublicKeyByIndex(0).toString());
+                } else if (command.equals("get_display_name")) {
+                    this.outputText.setText(card.getDisplayName());
+                } else if (command.equals("set_display_name")) {
+                    card.setDisplayName("杭州宇链科技有限公司");
+                } else {
+                    Toast.makeText(this, String.format("Command '%s' is not handled", command), Toast.LENGTH_LONG).show();
+                }
+            } else {
+                Toast.makeText(this, "Card is not available", Toast.LENGTH_LONG).show();
+            }
+        } catch (VCChipException e) {
+            Toast.makeText(this, e.toString(), Toast.LENGTH_LONG).show();
+        }
     }
 
     @Override
@@ -42,14 +89,7 @@ public class MainActivity extends AppCompatActivity implements CardManager.OnCar
 
     @Override
     public boolean onCardSwipe(CardManager cardManager, Card card) {
-        try {
-            String key0 = card.getPublicKeyByIndex(0).toString();
-            String key1 = card.getPublicKeyByIndex(1).toString();
-
-            Toast.makeText(this, String.format("%s, %s", key0, key1), Toast.LENGTH_LONG).show();
-        } catch (VCChipException e) {
-            e.printStackTrace();
-        }
+        this.card = card;
 
         return false;
     }
--- sdk/src/main/java/ltd/vastchain/evericard/sdk/Card.java
+++ sdk/src/main/java/ltd/vastchain/evericard/sdk/Card.java
@@ -1,7 +1,16 @@
 package ltd.vastchain.evericard.sdk;
 
+import org.apache.commons.lang.StringEscapeUtils;
+import org.apache.commons.lang3.ArrayUtils;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+
 import io.everitoken.sdk.java.PublicKey;
+import io.everitoken.sdk.java.Utils;
 import ltd.vastchain.evericard.sdk.channels.EveriCardChannel;
+import ltd.vastchain.evericard.sdk.command.ConfigurationRead;
+import ltd.vastchain.evericard.sdk.command.ConfigurationWrite;
 import ltd.vastchain.evericard.sdk.command.PublicKeyRead;
 import ltd.vastchain.evericard.sdk.response.Response;
 
@@ -19,9 +28,38 @@ public class Card {
         Response res = new Response(ret);
 
         if (!res.isSuccessful()) {
-            throw new VCChipException("get_publicKey_fail", "could not get public key");
+            throw new VCChipException("get_publicKey_fail", String.format("could not get public key at index %s", keyIndex));
         }
 
         return new PublicKey(res.getContent());
+    }
+
+    public String getDisplayName() throws VCChipException {
+        ConfigurationRead read = ConfigurationRead.readConfigurationItemData((byte) 0x0a);
+
+        byte[] ret = channel.sendCommand(read);
+        Response res = new Response(ret);
+
+        if (!res.isSuccessful()) {
+            throw new VCChipException("get_display_name_failed", "Failed to get display name");
+        }
+
+        String utf = new String(ArrayUtils.subarray(res.getContent(), 4, res.getContent().length), StandardCharsets.UTF_8);
+        return StringEscapeUtils.unescapeXml(utf);
+    }
+
+    public void setDisplayName(String name) throws VCChipException {
+        // todo validate name a bit
+
+        ConfigurationWrite command = ConfigurationWrite.configureSettings(Arrays.asList(
+                ConfigurationWrite.createTLVSetting((byte) 0x0a, StringEscapeUtils.escapeXml(name).getBytes())
+        ), true);
+
+        byte[] ret = channel.sendCommand(command);
+        Response res = new Response(ret);
+
+        if (!res.isSuccessful()) {
+            throw new VCChipException("set_display_name_failed", String.format("Failed to set display name to %s (%s).", name, Utils.HEX.encode(res.getStatus())));
+        }
     }
 }
