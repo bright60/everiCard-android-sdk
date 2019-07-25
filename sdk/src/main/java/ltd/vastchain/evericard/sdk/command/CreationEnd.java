2d515ec6cd40a1a9cf8cc189dfaa25855dac424b
Fei Liu <liu.feiwood@gmail.com>
2019-07-25 17:12:31 +0200

    add creationEnd command

---
+++ sdk/src/main/java/ltd/vastchain/evericard/sdk/command/CreationEnd.java
@@ -0,0 +1,10 @@
+package ltd.vastchain.evericard.sdk.command;
+
+public class CreationEnd extends Command {
+    public static byte CLA = (byte) 0x80;
+    public static byte INS = (byte) 0x0c;
+
+    public CreationEnd(byte[] data) {
+        super(CLA, INS, (byte) 0x00, (byte) 0x00, (byte) 0x00);
+    }
+}
